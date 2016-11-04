35b4e7361c93c4f4d129c04af2de81db5eab61eb
Arnaud Boulay <arnaud.boulay@epitech.eu>
2016-11-04 17:23:31 +0100

    Add QT tests to compile test

--- tests/compile/test.cpp
+++ tests/compile/test.cpp
@@ -1,10 +1,33 @@
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/vector.hpp>
+
+#include <QtGui>
+
+#include <fstream>
 #include <cstdlib>
 
-int             main(void)
+int             main(int argc, char **argv)
 {
-  boost::asio::io_service io_service;
+  std::ifstream ifile("/tmp/test.txt");
 
-  return (EXIT_SUCCESS);
+  // BOOST
+  boost::asio::io_service io_service;
+  try {
+    boost::archive::binary_iarchive iTextArchive(ifile);
+  } catch (boost::archive::archive_exception) {
+  }
+
+
+  // QT
+  QApplication app(argc, argv);
+  QWidget window;
+  window.resize(320, 240);
+  window.show();
+  window.setWindowTitle(QApplication::translate("toplevel", "Top-level widget"));
+  return app.exec();
 }
