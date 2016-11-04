9c25068975a1dda16a69847a201401c41ef97543
Arnaud Boulay <arnaud.boulay@epitech.eu>
2016-11-04 23:16:44 +0100

    Add Opus and PortAudio tests

--- tests/compile/test.cpp
+++ tests/compile/test.cpp
@@ -8,15 +8,18 @@
 
 // Ignore Warning
 #ifdef _MSC_VER
-#pragma warning(disable: 4127) // disable warning 4345
+#pragma warning(disable: 4127) // disable warning 4127
 #endif
 
 #include <QtGui>
 
 // Enable Warning
 #ifdef _MSC_VER
-#pragma warning(default: 4127) // disable warning 4345
+#pragma warning(default: 4127) // disable warning 4127
 #endif
+
+#include "portaudio.h"
+#include "opus.h"
 
 #include <fstream>
 #include <cstdlib>
@@ -25,13 +28,18 @@ int             main(int argc, char **argv)
 {
   std::ifstream ifile("/tmp/test.txt");
 
+  // PortAudio
+  Pa_Initialize();
+  
+  // OPUS
+  opus_encoder_destroy(nullptr);
+  
   // BOOST
   boost::asio::io_service io_service;
   try {
     boost::archive::binary_iarchive iTextArchive(ifile);
   } catch (boost::archive::archive_exception) {
   }
-
 
   // QT
   QApplication app(argc, argv);
