85de3b69f424d93c54fbd673d3ccf11696cac530
Arnaud Boulay <arnaud.boulay@epitech.eu>
2016-11-07 12:39:46 +0100

    Add missing string header

--- includes/Protocol.hpp
+++ includes/Protocol.hpp
@@ -3,6 +3,8 @@
 
 # define MAGIC_NUMBER   0xDEAD
 
+# include <string>
+
 namespace Protocol
 {
   struct          BabelPacket
@@ -10,35 +12,35 @@ namespace Protocol
     // Code enum
     enum class Code : unsigned int
       {
-	NONE = 00,
-	// Requests
-	SIGN_IN = 100,
-	SIGN_UP = 101,
-	SIGN_OUT = 102,
-	CONTACT_LIST_REQUEST = 103,
-	CALL = 104,
-	ADD_CONTACT = 105,
-	DEL_CONTACT = 106,
-	UPDATE_CONTACT_STATUS = 107,
-	// Errors
-	INVALID_REQUEST = 1,
-	USER_NOT_FOUND = 2,
-	WRONG_PASSWORD = 3,
-	USER_ALREADY_EXIST = 4,
-	NOT_LOGGED_IN = 5,
-	USER_DOES_NOT_EXIST = 6,
-	DECLINE_CALL = 7,
-	ALREADY_A_FRIEND = 8,
-	FRIEND_NOT_FOUND = 9,
-	// Success
-	LOGIN_SUCCESS = 200,
-	SIGN_UP_SUCCESS = 201,
-	DISCONNECT_SUCCESS = 202,
-	CONTACT_LIST = 203,
-	ACCEPT_CALL = 204,
-	CONTACT_ADDED = 205,
-	CONTACT_DELETED = 205,
-	ID_ADDED = 206,
+        NONE = 00,
+        // Requests
+        SIGN_IN = 100,
+        SIGN_UP = 101,
+        SIGN_OUT = 102,
+        CONTACT_LIST_REQUEST = 103,
+        CALL = 104,
+        ADD_CONTACT = 105,
+        DEL_CONTACT = 106,
+        UPDATE_CONTACT_STATUS = 107,
+        // Errors
+        INVALID_REQUEST = 1,
+        USER_NOT_FOUND = 2,
+        WRONG_PASSWORD = 3,
+        USER_ALREADY_EXIST = 4,
+        NOT_LOGGED_IN = 5,
+        USER_DOES_NOT_EXIST = 6,
+        DECLINE_CALL = 7,
+        ALREADY_A_FRIEND = 8,
+        FRIEND_NOT_FOUND = 9,
+        // Success
+        LOGIN_SUCCESS = 200,
+        SIGN_UP_SUCCESS = 201,
+        DISCONNECT_SUCCESS = 202,
+        CONTACT_LIST = 203,
+        ACCEPT_CALL = 204,
+        CONTACT_ADDED = 205,
+        CONTACT_DELETED = 205,
+        ID_ADDED = 206,
       };
 
     // Infos
@@ -54,8 +56,8 @@ namespace Protocol
   class Protocol
   {
   public:
-	static struct BabelPacket *createPacket(BabelPacket::Code code, unsigned char *data, unsigned int const &length);
-	static unsigned char      *stringToPointer(std::string const &);
+        static struct BabelPacket *createPacket(BabelPacket::Code code, unsigned char *data, unsigned int const &length);
+        static unsigned char      *stringToPointer(std::string const &);
   };
 
 }
